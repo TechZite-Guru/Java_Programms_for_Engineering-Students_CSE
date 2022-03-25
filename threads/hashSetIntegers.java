02498372ffc49bfa382322cc443c212e0e18bbb9
Aravind Babu <o161800@rguktrkv.ac.in>
2022-03-26 00:38:05 +0530

    Create hashSetIntegers.java

---
+++ threads/hashSetIntegers.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+ 
+class hashSetIntegers {
+   
+    public static void main(String[] args)
+    {
+        // Instantiate an object of HashSet
+        HashSet<Integer> hashset = new HashSet<Integer>();
+ 
+        // Add elements using add method
+        for (int i = 1; i<=100; i++) {
+            hashset.add(i);
+        }
+      
+        System.out.println(set.size());
+    }
+}
