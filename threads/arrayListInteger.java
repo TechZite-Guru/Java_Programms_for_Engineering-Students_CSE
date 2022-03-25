ef6fd5fb358d105a46b60b15670cd5bbb1ca8cfb
Aravind Babu <o161800@rguktrkv.ac.in>
2022-03-26 00:57:20 +0530

    Update arrayListInteger.java

--- threads/arrayListInteger.java
+++ threads/arrayListInteger.java
@@ -5,14 +5,21 @@ class arrayListIntegers {
    
     public static void main(String[] args)
     {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter No.of Integers to Store: ");
+        int n = s.nextInt();
+     
         // create ArrayList list1
         ArrayList<Integer> list = new ArrayList<>();
  
         // Add elements using add method
-        for (int i = 1; i<=100; i++) {
-            list.add(i);
+        for (int i = 1; i<= n; i++) {
+            System.out.println("Enter "+i+" Integer to Store: ");
+            int m = s.nextInt();
+            list.add(m);
         }
       
-        System.out.println(list.size());
+        System.out.println("Resultant Hashset: " +list);
     }
 }
