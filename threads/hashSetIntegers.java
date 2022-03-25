99773d145a9c3043221c384ede1f9d3d79022b45
Aravind Babu <o161800@rguktrkv.ac.in>
2022-03-26 00:57:59 +0530

    Update hashSetIntegers.java

--- threads/hashSetIntegers.java
+++ threads/hashSetIntegers.java
@@ -1,18 +1,27 @@
 import java.io.*;
 import java.util.*;
  
+import java.io.*;
+import java.util.*;
+ 
 class hashSetIntegers {
    
     public static void main(String[] args)
     {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter No.of Integers to Store: ");
+        int n = s.nextInt();
         // Instantiate an object of HashSet
-        HashSet<Integer> hashset = new HashSet<Integer>();
+        HashSet<Integer> set = new HashSet<Integer>();
  
         // Add elements using add method
-        for (int i = 1; i<=100; i++) {
-            hashset.add(i);
+        for (int i = 1; i<= n; i++) {
+            System.out.println("Enter "+i+" Integer to Store: ");
+            int m = s.nextInt();
+            set.add(m);
         }
       
-        System.out.println(set.size());
+        System.out.println("Resultant Hashset: " +set);
     }
 }
