005c8b09c4da76d6356497e7601fb4b1f6a8c28e
Aravind Babu <o161800@rguktrkv.ac.in>
2022-03-26 01:43:44 +0530

    Update queueInterface.java

--- threads/queueInterface.java
+++ threads/queueInterface.java
@@ -1,28 +1,42 @@
 import java.util.*;
  
-public class queueInterface {
+public class Main {
+    
+    static class Queue {
+        static Stack<Integer> stack1 = new Stack<Integer>();
+        static Stack<Integer> stack2 = new Stack<Integer>();
+        
+        static void enQueue(int x) {
+            stack1.push(x);
+        }
+        
+        static void deQueue(int x) {
+            stack2.push(stack1.pop(x));
+        }
+        
+    }
  
     public static void main(String args[])
     {
         Scanner s = new Scanner(System.in);
-        Queue<Integer> queue = new PriorityQueue<>();
-        Queue<Integer> queue2 = new PriorityQueue<>();
  
+        Queue q = new Queue();
+        
         for(int i = 1; i<=6; i++ ) {
             System.out.println("Enter "+i+" Integer to add: ");
             int k = s.nextInt();
-            queue.add(k);
+            q.enQueue(k);
         }
         
-        System.out.println("Initial Queue: " + queue);
+        System.out.println("Initial Queue: " + stack1);
         
         for(int i = 1; i<=2; i++ ) {
             System.out.println("Enter Integer to remove: ");
             int k = s.nextInt();
-            queue.remove(k);
-            queue2.add(k);
+            q.deQueue(k);
         }
-        System.out.println("Removed Elements: " + queue2);
-        System.out.println("Existing Elements: " + queue);
+        
+        System.out.println("Removed Elements: " + stack2);
+        System.out.println("Existing Elements: " + stack1);
     }
 }
