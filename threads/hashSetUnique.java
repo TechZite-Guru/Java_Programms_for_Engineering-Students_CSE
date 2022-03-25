48781480a82cd83991f11d1849ca02427c4eb6d6
Aravind Babu <o161800@rguktrkv.ac.in>
2022-03-26 00:53:26 +0530

    Create hashSetUnique.java

---
+++ threads/hashSetUnique.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+ 
+class hashSetIntegers {
+   
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter No.of Integers to Store: ");
+        int n = s.nextInt();
+        // Instantiate an object of HashSet
+        HashSet<Integer> set = new HashSet<Integer>();
+ 
+        // Add elements using add method
+        for (int i = 1; i<= n; i++) {
+            System.out.println("Enter "+i+" Integer to Store: ");
+            int m = s.nextInt();
+            set.add(m);
+        }
+      
+        System.out.println("Resultant Hashset: " +set);
+    }
+}
